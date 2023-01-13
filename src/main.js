95ba3c4c9389102e3eb54e76a101e9d404ba3f9a
giorgioGTelian <giorgio.programmer@gmail.com>
2023-01-13 12:33:17 +0100

    2 files modified, 1 added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-const { createApp } = require('vue');
+import Vue from "vue";
 import App from "./App.vue";
+import store from "./store/store";
 
-createApp(App).mount("#app");
+
+Vue.config.productionTip = false;
+Vue.filter("round", function(value, decimals) {
+  if (!value) {
+    value = 0;
+  }
+
+  if (!decimals) {
+    decimals = 0;
+  }
+
+  value = Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
+  return value;
+});
+
+new Vue({
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
